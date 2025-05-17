415921d0dc7b3ac501fad09c474680ca1f55e8df
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-05-17 11:06:04 +0200

    feat: add constants for API URLs, Config and UI Text

---
+++ constants/index.ts
@@ -0,0 +1,12 @@
+// API URLs
+export const API_URL = "htts://example.com/api";
+
+// Config
+export const DEFAULT_TIMEOUT = 5000;
+
+// UI Text
+export const UI_TEXT = {
+  BUTTON_SUBMIT: "Submit",
+  BUTTON_CANCEL: "Cancel",
+  CARD_NO_IMAGE: "No image available",
+};
