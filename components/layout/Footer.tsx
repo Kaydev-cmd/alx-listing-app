599c4a7768600d0cf4371a57a23f1acae6709081
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-05-31 16:50:57 +0200

    feat: add basic Footer component with logo and link to home

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <div>
+        <a href="/">
+          <img
+            src="./assets/Vector.png"
+            alt="ALX Logo"
+            className="h-8 w-auto"
+          />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
